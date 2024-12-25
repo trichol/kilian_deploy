62bf3521cb14b34b6654c50daa8f3b514f6700d9
user267452 <trichol@gmail.com>
2024-12-25 16:33:21 +0000

    init3

--- runKilian.sh
+++ runKilian.sh
@@ -1,7 +1,11 @@
-cd $HOME/Documents/WORKSPACE
-rm -rf kilian_deploy
-git clone https://github.com/trichol/kilian_deploy.git
+cd $HOME/Documents/WORKSPACE/kilian_deploy
+git reset --hard HEAD
+git pull
 git log --oneline -5
+
+
+
+
 
 cd $HOME/Documents/WORKSPACE/kilian
 git reset --hard HEAD
@@ -13,7 +17,7 @@ git log --oneline -5
 rm -rf $HOME/Documents/WORKSPACE/kilian/ios/Runner/Assets.xcassets
 
 
-cp -R  $HOME/Documents/WORKSPACE/kilian_image/Assets.xcassets   $HOME/Documents/WORKSPACE/kilian/ios/Runner/Assets.xcassets
+cp -R  $HOME/Documents/WORKSPACE/kilian_deploy/Assets.xcassets   $HOME/Documents/WORKSPACE/kilian/ios/Runner/Assets.xcassets
 
 
 xcrun simctl uninstall booted com.freepartners.kilian
