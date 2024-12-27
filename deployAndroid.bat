4389e6c3f4a8c9c8113da2e9c95840abaad6e12a
trichol <147633004+trichol@users.noreply.github.com>
2024-12-27 21:23:21 +0100

    fix

--- deployAndroid.bat
+++ deployAndroid.bat
@@ -1,22 +1,64 @@
 @echo off
-:: Define the function to replace directories
-set SOURCE_DIR=E:\WORK\PROJETS\WORKSPACE\kilian_flutter_export
-set DEST_DIR=E:\WORK\PROJETS\WORKSPACE
+:: Define the base directories
+set "SOURCE_DIR=E:\WORK\PROJETS\WORKSPACE\kilian_flutter_export"
+set "DEST_DIR=E:\WORK\PROJETS\WORKSPACE\kilian_flutter_export"
+
+echo Source Directory: %SOURCE_DIR%
+echo Destination Directory: %DEST_DIR%
+
+cd /d %DEST_DIR%\kilian
+
+git reset --hard HEAD
+git pull
+git log --oneline -5
+ 
+
+:: Call the function for each directory
+call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-hdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-hdpi"
+call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-mdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-mdpi"
+call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-xhdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-xhdpi"
+call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-xxhdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-xxhdpi"
+call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-xxxhdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-xxxhdpi"
 
 
+
+
+REM Build APK in release mode
+flutter build apk --release
+
+REM Build AppBundle in release mode
+flutter build appbundle --release
+
+keytool -printcert -jarfile  %DEST_DIR%\build\app\outputs\flutter-apk\app-release.apk
+
+REM Pause to see the output
+pause
+
+
+:: Function to replace directories
 :replace_directory
 REM Parameters: %1 = Source Directory, %2 = Destination Directory
-set "source=%1"
-set "destination=%2"
+set "source=%~1"
+set "destination=%~2"
 
-echo Replacing %destination% with %source%...
+REM Debugging output
+echo Source: "%source%"
+echo Destination: "%destination%"
 
-REM Remove the destination directory and its contents
+REM Check if source exists
+if not exist "%source%" (
+    echo Error: Source directory does not exist: "%source%"
+    exit /b
+)
+
+REM Remove the destination directory
 if exist "%destination%" (
+    echo Removing existing destination: "%destination%"
     rmdir /S /Q "%destination%"
 )
 
-REM Copy the source directory to the destination
+REM Copy source to destination
+echo Copying files from "%source%" to "%destination%"...
 xcopy "%source%" "%destination%" /E /H /C /I
 
 if %ERRORLEVEL% EQU 0 (
@@ -26,13 +68,7 @@ if %ERRORLEVEL% EQU 0 (
 )
 
 exit /b
-:: End of function
 
-:: Call the function
-call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-hdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-hdpi"
-call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-mdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-mdpi"
-call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-xhdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-xhdpi"
-call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-xxhdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-xxhdpi"
-call :replace_directory "%SOURCE_DIR%\kilian_deploy\android\mipmap-xxxhdpi" "%DEST_DIR%\kilian\android\app\src\main\res\mipmap-xxxhdpi"
+:: End of function
 
 pause
