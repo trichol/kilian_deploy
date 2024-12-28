57394867f917a5145af012cfa75a1727bdfe1870
trichol <147633004+trichol@users.noreply.github.com>
2024-12-28 20:18:32 +0100

    ftp ios

--- deployAndroid.bat
+++ deployAndroid.bat
@@ -30,6 +30,23 @@ REM Build AppBundle in release mode
 flutter build appbundle --release
 
 
+rem Path to WinSCP executable
+set WINSCP_PATH="C:\Program Files (x86)\WinSCP\WinSCP.com"
+rem Path to script file
+set SCRIPT_PATH=%SOURCE_DIR%\kilian_deploy\upload_script.txt
+
+rem Run WinSCP with the script
+%WINSCP_PATH% /script=%SCRIPT_PATH%
+
+rem Check if the upload was successful
+if %errorlevel% equ 0 (
+    echo Upload succeeded.
+) else (
+    echo Upload failed.
+)
+
+
+
 
 REM Pause to see the output
 pause
--- deployIOS.sh
+++ deployIOS.sh
@@ -20,4 +20,23 @@ git log --oneline -5
 
 
 xcrun simctl uninstall booted com.freepartners.kilian
+
+
+# Variables
+FTP_SERVER="yr3bv.ftp.infomaniak.com"
+FTP_LOGIN="yr3bv_trichol"
+FTP_PASSWORD="Taitai44!"
+LOCAL_FILE=$HOME/Documents/WORKSPACE/kilian
+
+# Upload file using curl
+curl -T "$LOCAL_FILE" "ftp://$FTP_SERVER/" --user "$FTP_LOGIN:$FTP_PASSWORD"
+
+# Check if upload was successful
+if [ $? -eq 0 ]; then
+  echo "File uploaded successfully to $FTP_SERVER"
+else
+  echo "Failed to upload file to $FTP_SERVER"
+fi
+
+
 # flutter run -d 626CA41B-EBD2-4293-A575-7538FC735A5F
